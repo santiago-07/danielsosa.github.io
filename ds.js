3a0a69b1ef1de3761ddd24efa65ba3122ac92a39
santiago-07 <125928260+santiago-07@users.noreply.github.com>
2023-08-29 20:58:03 -0300

    Add files via upload

---
+++ ds.js
@@ -0,0 +1,11 @@
+document.getElementById("div_2").addEventListener("mouseover", mouseover);
+document.getElementById("div_2").addEventListener("mouseout", mouseout);
+
+function mouseover() {
+    document.getElementById("div_2").style.color = "white";
+}
+
+function mouseout() {
+    document.getElementById("div_2").style.color = "black";
+}
+
